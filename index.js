3f2eaa25f7ee74cd95a12aef7e0327725254580f
Wael Joseph Murad <waelmuradian@outlook.com>
2019-01-27 20:43:12 -0800

    modifying index.js file

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 class Stopwatch {
-    constructor(display) {
+    constructor(display, startpause) {
         this.running = false;
         this.display = display;
         this.reset();
         this.print(this.times);
+        this.startpause = startpause;
     }
 
     reset() {
@@ -17,18 +18,21 @@ class Stopwatch {
             requestAnimationFrame(this.step.bind(this));
         }
     }
+
     stop() {
         this.running = false;
         this.time = null;
+        this.startpause.innerText = "Resume";
     }
 
     restart() {
-            this.running = false;
-            this.time=null;
-            this.reset();
-            this.display.innerText="00:00:00"
-        }
-        
+        this.running = false;
+        this.time = null;
+        this.reset();
+        this.display.innerText = "00:00:00";
+        this.startpause.innerText = "Start";
+    }
+
 
     step(timestamp) {
         if (!this.running) return;
@@ -37,18 +41,13 @@ class Stopwatch {
         this.print();
         requestAnimationFrame(this.step.bind(this));
     }
-
-
     calculate(timestamp) {
         var diff = timestamp - this.time;
-        // Hundredths of a second are 100 ms
         this.times[2] += diff / 10;
-        // Seconds are 100 hundredths of a second
         if (this.times[2] >= 100) {
             this.times[1] += 1;
             this.times[2] -= 100;
         }
-        // Minutes are 60 seconds
         if (this.times[1] >= 60) {
             this.times[0] += 1;
             this.times[1] -= 60;
@@ -65,7 +64,7 @@ ${pad0(times[0], 2)}:\
 ${pad0(times[1], 2)}:\
 ${pad0(Math.floor(times[2]), 2)}`;
     }
-    
+
 }
 
 function pad0(value, count) {
@@ -75,8 +74,52 @@ function pad0(value, count) {
     return result;
 }
 
+function clearChildren(node) {
+    while (node.lastChild)
+        node.removeChild(node.lastChild);
+}
 
 
-let stopwatch = new Stopwatch(
-    document.querySelector('.counter'));
+let stopwatch1 = new Stopwatch(
+    document.querySelector('#counter1'),
+    document.querySelector('#start-1'));
 
+let stopwatch2 = new Stopwatch(
+    document.querySelector('#counter2'),
+    document.querySelector('#start-2'));
+
+let stopwatch3 = new Stopwatch(
+    document.querySelector('#counter3'),
+    document.querySelector('#start-3'));
+
+
+function toggleWatch2() {
+    var d = document.getElementById("div-2");
+    var b = document.getElementById("btn-add-1")
+    if (d.style.display === "flex") {
+        d.style.display = "none";
+        b.innerText = "Add Watch";
+        stopwatch2.restart()
+    } else {
+        d.style.display = "flex";
+        b.innerText = "Delete Watch";
+
+    }
+
+
+}
+
+function toggleWatch3() {
+    var d = document.getElementById("div-3");
+    var b = document.getElementById("btn-add-2")
+    if (d.style.display === "flex") {
+        d.style.display = "none";
+        b.innerText = "Add Watch";
+        stopwatch3.restart()
+
+    } else {
+        d.style.display = "flex";
+        b.innerText = "Delete Watch";
+    }
+
+}
